dc4bb25c1063d47722560af91feb890cf8d89110
NoitaminAoki <s2.DanielAoki@gmail.com>
2024-08-31 21:43:09 +0700

    Update duo.js

--- scripts/turrets/duo.js
+++ scripts/turrets/duo.js
@@ -1,3 +1,10 @@
 //Enhancement Duo Turret
 Blocks.duo.reload = 10;
+Blocks.duo.range = 170;
+
 Blocks.duo.shoot.shots = 2;
+
+Blocks.duo.limitRange(70);
+Blocks.duo.ammoTypes.get(Items.copper).damage = 17;
+Blocks.duo.ammoTypes.get(Items.graphite).damage = 27;
+Blocks.duo.ammoTypes.get(Items.silicon).damage = 21;
