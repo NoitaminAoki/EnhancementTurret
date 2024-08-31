7ba228f8aaa34c1b3fa6be5ec7a97828359ce909
NoitaminAoki <s2.DanielAoki@gmail.com>
2024-08-31 21:48:25 +0700

    Update scatter.js

--- scripts/turrets/scatter.js
+++ scripts/turrets/scatter.js
@@ -1,6 +1,12 @@
 //Enhancement Scatter Turret
 Blocks.scatter.inaccuracy = 8;
 Blocks.scatter.reload = 40;
+Blocks.scatter.range = 270;
 
 Blocks.scatter.shoot.shotDelay = 3.5;
 Blocks.scatter.shoot.shots = 6;
+
+Blocks.scatter.limitRange(90);
+Blocks.scatter.ammoTypes.get(Items.lead).damage = 8;
+Blocks.scatter.ammoTypes.get(Items.metaglass).damage = 8;
+Blocks.scatter.ammoTypes.get(Items.scrap).damage = 8;
